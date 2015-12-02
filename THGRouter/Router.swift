897bb50c40fc192521a7b29e4889d192adf727f7
Angelo Di Paolo <angelod101@gmail.com>
2015-12-02 11:42:00 -0500

    make `evaluateURL` return true when a URL was successfully handled by a route

--- THGRouter/Router.swift
+++ THGRouter/Router.swift
@@ -13,9 +13,24 @@ import THGFoundation
 @objc
 public class Router: NSObject {
     static public let sharedInstance = Router()
-    
     public var staticNavigator: StaticNavigator? = nil
+    public private(set) var routes = [Route]()
     
+    private var translation = [String : String]()
+
+    public func translate(from: String, to: String) {
+        let existing = translation[from]
+        if existing != nil {
+            exceptionFailure("A translation for \(from) exists already!")
+        }
+        
+        translation[from] = to
+    }
+}
+
+// MARK: - Registering Routes
+
+extension Router {
     public func register(route: Route) {
         if route.name != nil {
             routes.append(route)
@@ -38,24 +53,20 @@ public class Router: NSObject {
             }
         }
     }
-    
-    public func translate(from: String, to: String) {
-        let existing = translation[from]
-        if existing != nil {
-            exceptionFailure("A translation for \(from) exists already!")
-        }
-        
-        translation[from] = to
+}
+
+// MARK: - Evaluating Routes
+
+extension Router {
+    public func evaluateURL(url: NSURL) -> Bool {
+        guard let components = url.deepLinkComponents else { return false }
+        return evaluate(components)
     }
     
-    public func evaluateURL(url: NSURL) {
-        if let components = url.deepLinkComponents {
-            evaluate(components)
-        }
-    }
-    
-    public func evaluate(components: [String]) {
+    public func evaluate(components: [String]) -> Bool {
         var route: Route? = nil
+        var routeWasExecuted = false
+        
         for i in 0..<components.count {
             let item = components[i]
             if i == 0 {
@@ -64,6 +75,7 @@ public class Router: NSObject {
                 if routes.count > 0 {
                     route = routes[0]
                     route?.execute(false)
+                    routeWasExecuted = true
                 } else {
                     break
                 }
@@ -71,16 +83,14 @@ public class Router: NSObject {
                 // TODO: Fill this in.
             }
         }
+        
+        return routeWasExecuted
     }
-    
-    public private(set) var routes = [Route]()
-    
-    private var translation = [String : String]()
 }
 
+// MARK: - Getting Routes
 
 extension Router {
-    
     public func routesByName(name: String) -> [Route] {
         let result = routes.filter { (item) -> Bool in
             if item.name == name {
