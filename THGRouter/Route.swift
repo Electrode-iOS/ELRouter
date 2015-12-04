460a095b222882d8ac38b020debc1cefba8a5d44
Angelo Di Paolo <angelod101@gmail.com>
2015-12-03 22:50:04 -0500

    refactor execute() with guards to cleanup nested let bindings

--- THGRouter/Route.swift
+++ THGRouter/Route.swift
@@ -58,50 +58,45 @@ public class Route: NSObject {
     }
     
     public func execute(animated: Bool, variable: String? = nil) -> UIViewController? {
-        var result: UIViewController? = nil
+        // bail out when missing a valid navigator or action
+        guard let navigator = parentRouter?.navigator else { return nil }
+        guard let action = action else { return nil }
         
-        if let navigator = parentRouter?.navigator {
-            if let action = self.action {
-                if (staticValue != nil) {
-                    result = staticValue
-                    parentRouter?.navigator?.selectedViewController = staticValue
-                } else {
-                    result = action(variable: variable)
-                    
-                    let navController = navigator.selectedViewController as? UINavigationController
-                    
-                    switch(type) {
-                    case .Static:
-                        // do nothing.  tab's are handled slightly differently above.
-                        // TODO: say some meaningful shit about why this works this way.
-                        staticValue = result
-                        break
-                        
-                    case .Screen:
-                        if let vc = result {
-                            navController?.pushViewController(vc, animated: animated)
-                        }
-                        break
-                        
-                    case .Modal:
-                        if let vc = result {
-                            if navController?.topViewController?.presentedViewController != nil {
-                                navController?.topViewController?.presentedViewController?.dismissViewControllerAnimated(animated) { () -> Void in
-                                    // do something in the completion block?
-                                }
-                            } else {
-                                navController?.topViewController?.presentViewController(vc, animated: animated) {
-                                    // do something in the completion block?
-                                }
-                            }
-                        }
-                        break
-                        
-                    default:
-                        break
-                    }
-                    
+        var result: UIViewController? = nil
+
+        if let staticValue = staticValue {
+            result = staticValue
+            parentRouter?.navigator?.selectedViewController = staticValue
+        } else {
+            result = action(variable: variable)
+            
+            let navController = navigator.selectedViewController as? UINavigationController
+            
+            switch(type) {
+            case .Static:
+                // do nothing.  tab's are handled slightly differently above.
+                // TODO: say some meaningful shit about why this works this way.
+                staticValue = result
+                
+            case .Screen:
+                if let vc = result {
+                    navController?.pushViewController(vc, animated: animated)
                 }
+                
+            case .Modal:
+                if let vc = result {
+                    if navController?.topViewController?.presentedViewController != nil {
+                        navController?.topViewController?.presentedViewController?.dismissViewControllerAnimated(animated) { () -> Void in
+                            // do something in the completion block?
+                        }
+                    } else {
+                        navController?.topViewController?.presentViewController(vc, animated: animated) {
+                            // do something in the completion block?
+                        }
+                    }
+                }
+                
+            default: break
             }
         }
         
