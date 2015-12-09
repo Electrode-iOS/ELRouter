135f083a9f732fbc2d7f07e3b5de38bc9c6c0ea8
Angelo Di Paolo <angelod101@gmail.com>
2015-12-09 16:13:41 -0500

    add method to get end routes for a given URL

--- THGRouter/Router.swift
+++ THGRouter/Router.swift
@@ -210,4 +210,25 @@ extension Router {
     public func routesByType(type: RoutingType) -> [Route] {
         return routes.filter { return $0.type == type }
     }
+    
+    /// Find the end routes that satisfy a URL
+    public func routeSetForURL(url: NSURL) -> [Route]? {
+        guard let components = url.deepLinkComponents else { return nil }
+        
+        var currentRouteSet: [Route] = [masterRoute]
+        
+        for index in 0..<components.count {
+            let part = components[index]
+            
+            var routeSet = [Route]()
+            
+            for route in currentRouteSet {
+                route.routesByName(part).each { routeSet.append($0) }
+            }
+            
+            currentRouteSet = routeSet
+        }
+        
+        return currentRouteSet
+    }
 }
