feb43c2ff55198254e3794e6bc880a468bf84296
Brandon Sneed <brandon@redf.net>
2015-12-09 13:29:40 -0800

    Added stuff around swizzling navigation bits.

---
+++ THGRouter/NavigationSyncing.swift
@@ -0,0 +1,50 @@
+//
+//  NavigationSyncing.swift
+//  THGRouter
+//
+//  Created by Brandon Sneed on 10/15/15.
+//  Copyright © 2015 theholygrail.io. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UINavigationController {
+}
+
+extension UIViewController {
+    public override class func initialize() {
+        struct Static {
+            static var token: dispatch_once_t = 0
+        }
+        
+        // make sure this isn't a subclass
+        if self !== UIViewController.self {
+            return
+        }
+        
+        dispatch_once(&Static.token) {
+            let originalSelector = Selector("viewDidAppear:")
+            let swizzledSelector = Selector("router_viewDidAppear:")
+            
+            let originalMethod = class_getInstanceMethod(self, originalSelector)
+            let swizzledMethod = class_getInstanceMethod(self, swizzledSelector)
+            
+            let didAddMethod = class_addMethod(self, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
+            
+            if didAddMethod {
+                class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
+            } else {
+                method_exchangeImplementations(originalMethod, swizzledMethod);
+            }
+        }
+    }
+    
+    // MARK: - Method Swizzling
+    
+    func router_viewDidAppear(animated: Bool) {
+        self.router_viewDidAppear(animated)
+        
+        // release whatever lock is present
+    }
+}
